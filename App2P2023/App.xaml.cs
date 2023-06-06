e6cd5f6c0b9eb99ed447923b2dd945a0719aa40d
Amner1997 <60415793+Amner1997@users.noreply.github.com>
2023-06-06 15:31:45 -0600

    Agregar archivos de proyecto.

---
+++ App2P2023/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2P2023
+{
+    public partial class App : Application
+    {
+
+        static Controllers.DBProc dBProc;
+
+        public static Controllers.DBProc Instancia
+        {
+            get 
+            { 
+                if(dBProc == null)
+                {
+                    String dbname = "Proc.db3";
+                    String dbpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    String dbfulpath = Path.Combine(dbpath, dbname);
+                    dBProc = new Controllers.DBProc(dbfulpath);
+                }
+
+                return dBProc; 
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage( new Views.PageListEmple());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ App2P2023/Models/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using App2P2023.Models;
+using SQLite;
+
+namespace App2P2023.Models
+{
+    public class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Nombres { get; set; }
+
+        [MaxLength(100)]
+        public string Apellidos { get; set; }
+
+        public DateTime FechaNac { get; set; }
+
+        [MaxLength (20), Unique]
+        public string Telefono { get; set; }
+
+        public byte[] foto { get; set; }
+    }
+}
